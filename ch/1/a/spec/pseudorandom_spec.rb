4aca32f681e4ccc7b58161b8ab611fe50f2de43a
Greg Kostin <gkostin@umich.edu>
2018-05-04 12:23:30 -0400

    resolution

--- ch/1/a/spec/pseudorandom_spec.rb
+++ ch/1/a/spec/pseudorandom_spec.rb
@@ -13,6 +13,7 @@ require 'pseudorandom'
 RSpec.describe Pseudorandom do
   let(:pseudorandom) { described_class.new(seed) }
   let(:seed) { Random.rand }
+  let(:debug) { Debug.new }
 
   describe 'random' do
     it "returns a number >= 0.0 and <= 1.0" do
@@ -20,6 +21,38 @@ RSpec.describe Pseudorandom do
       1000.times do
         number = pseudorandom.random
         expect(number).to be_between(0, 1)
+      end
+    end
+  end
+
+  describe 'quartiles' do
+    let(:min) { 0.25 - epsilon }
+    let(:max) { 0.25 + epsilon }
+    let(:epsilon) { 0.03 }
+    let(:n) { 1000 }
+    let(:quartiles) { [0, 0, 0, 0] }
+
+    2.times do |t|
+      it "test ##{t}" do
+        n.times do
+          value = pseudorandom.random
+          if value < 0.25
+            quartiles[0] += 1
+          elsif value < 0.5
+            quartiles[1] += 1
+          elsif value < 0.75
+            quartiles[2] += 1
+          else
+            quartiles[3] += 1
+         end
+        end
+        quartiles.map! { |v| v / n.to_f }
+        4.times do |i|
+          debug.puts "quartile #{i}  #{min} > #{quartiles[i]} < #{max}"
+          expect(quartiles[i]).to be_between(min, max)
+        end
+        debug.puts "quartiles sum #{quartiles.sum}"
+        expect(quartiles.sum).to be_between(0.0, 1.0)
       end
     end
   end
