3b3590aa635f115496df3c4513468c1ca8b3e53d
Greg Kostin <gkostin@umich.edu>
2018-05-04 09:43:21 -0400

    Chapter 1: A. pseudorandom numbers

--- ch/1/a/lib/pseudorandom.rb
+++ ch/1/a/lib/pseudorandom.rb
@@ -5,64 +5,16 @@ require_relative '../../../../lib/knuth'
 # Chapter 1 / A Gentle Introduction to Genetic Algorithms
 # COMPUTER ASSIGNMENTS A. ... pseudorandom numbers. ...
 
-# Quartiles
-class Quartiles
-  attr_reader :tries
-  attr_reader :under
-  attr_reader :first
-  attr_reader :second
-  attr_reader :third
-  attr_reader :forth
-  attr_reader :over
-
-  def initialize
-    @tries = 0
-    @under = 0
-    @first = 0
-    @second = 0
-    @third = 0
-    @forth = 0
-    @over = 0
-  end
-
-  def add(value) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-    @tries += 1
-    if value < 0
-      @under += 1
-    elsif value < 0.25
-      @first += 1
-    elsif value < 0.50
-      @second += 1
-    elsif value < 0.75
-      @third += 1
-    elsif value <= 1
-      @forth += 1
-    else
-      @over += 1
-    end
-  end
-end
-
 # Pseudorandom
 class Pseudorandom
   attr_reader :knuth
 
-  def initialize(seed)
+  def initialize(seed = Random.rand)
     @knuth = Knuth.new
     @knuth.warmup_random(seed)
   end
 
   def random
     @knuth.random
-  end
-
-  def track(tries)
-    results = Quartiles.new
-
-    tries.times do
-      results.add(Random.rand)
-    end
-
-    results
   end
 end
--- ch/1/a/spec/pseudorandom_spec.rb
+++ ch/1/a/spec/pseudorandom_spec.rb
@@ -12,32 +12,15 @@ require 'pseudorandom'
 
 RSpec.describe Pseudorandom do
   let(:pseudorandom) { described_class.new(seed) }
-  let(:seed) { 0.25 }
+  let(:seed) { Random.rand }
 
   describe 'random' do
-    it do
+    it "returns a number >= 0.0 and <= 1.0" do
       pseudorandom
       1000.times do
         number = pseudorandom.random
         expect(number).to be_between(0, 1)
       end
-    end
-  end
-
-  describe 'tracker' do
-    let(:results) { pseudorandom.track(tries) }
-    let(:tries) { 1000 }
-    let(:tolerance) { (tries / 4) + (tries * 0.02) }
-
-    it 'sums to 1000' do # rubocop:disable RSpec/MultipleExpectations, RSpec/ExampleLength
-      expect(results.tries).to eq(tries)
-      expect(results.under).to eq(0)
-      expect(results.over).to eq(0)
-      expect(results.first + results.second + results.third + results.forth).to eq(tries)
-      expect(results.first).to be < tolerance
-      expect(results.second).to be < tolerance
-      expect(results.third).to be < tolerance
-      expect(results.forth).to be < tolerance
     end
   end
 end
